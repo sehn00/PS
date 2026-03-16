b5e42c4b92082c39fb66bb57f0269389ea3fb65e
Sehyeon Choi <135334069+sehn00@users.noreply.github.com>
2026-03-16 17:58:01 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -6,32 +6,35 @@ int main()
     ios::sync_with_stdio(0); cin.tie(0);
     
     while (1) {
-        string S;
+        stack<int> st;
         bool ok = 1;
-        stack<char> st;
+        string S;
         getline(cin, S);
         if (S == ".") break;
         
         for (char c : S) {
             if (c == '(' || c == '[')
-                st.push(c);
+                st.push(c);    // 여는 괄호는 스택에 저장
             else if (c == ')') {
                 if (st.empty() || st.top() != '(') {
                     ok = 0;
-                    break;
-                }
-                else st.pop();
+                    break;      // 닫는 괄호 들어왔는데
+                }               // 스택 비거나, 짝이 안맞으면 break
+                else st.pop();  // 짝 맞으면 pop()
             }
             else if (c == ']') {
                 if (st.empty() || st.top() != '[') {
                     ok = 0;
-                    break;
+                    break;      // 마찬가지
                 }
                 else st.pop();
             }
         }
-        if (ok == 1 && st.empty()) cout << "yes\n";
-        else cout << "no\n";
+        if (!st.empty()) ok = 0;
+        // 문자열 끝까지 돌았는데 스택 안비었으면 0
+
+        if (ok == 0) cout << "no\n";
+        else cout << "yes\n";
     }
     return 0;
 }
