47a116541ae1a111e02e0e631d18ceb0830b26e9
Sehyeon Choi <135334069+sehn00@users.noreply.github.com>
2026-03-16 18:02:46 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dist[100002];
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+    
+    int n, k;
+    cin >> n >> k;
+    
+    fill(dist, dist+100001, -1);
+    
+    queue<int> Q;
+    Q.push(n);
+    dist[n] = 0;
+    
+    while (dist[k] == -1) {
+        int cur = Q.front();
+        Q.pop();
+        
+        for (int next : {cur-1, cur+1, cur*2}) {
+            if (next<0 || next>100000) continue;
+            if (dist[next] != -1) continue;
+            Q.push(next);
+            dist[next] = dist[cur] + 1;
+        }
+    }
+    cout << dist[k] << '\n';
+    
+    return 0;    
+}
